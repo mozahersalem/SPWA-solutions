22a24ef3ef3d7a59028cb477dc57de9860107d54
Mohamed Salem <mo.zaher.salem@gmail.com>
2016-10-23 10:19:34 +0200

    add functionality and working good

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -1,36 +1,76 @@
 (function () {
 'use strict';
 
-  angular.module('LunchCheck', [])
-  .controller('LunchCheckController', LunchCheckController);
+  angular.module('ShoppingListCheckOff', [])
+  .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController)
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-  LunchCheckController.$inject = ['$scope', '$filter'];
+  
+  ToBuyController.$inject = ['ShoppingListCheckOffService'];
+  function ToBuyController(ShoppingListCheckOffService) {
+    var toBuy = this;
+  
+    toBuy.items = ShoppingListCheckOffService.listToBuy;
 
-  function LunchCheckController($scope) {
-
-    $scope.parseInput = function () {
-      if($scope.userInput == undefined){
-        $scope.message = "Please enter data first";
-        $scope.userInput = '';
-      }
-
-      /* in this split method the empty comma consider a value
-       * item 1, item2,,item3 is Too much
-      */
-      
-      var inputSplit = $scope.userInput.split(',');
-      if(inputSplit == '') {
-        $scope.message = "Please enter data first";
-        $scope.messgeColor = "red";
-      }else if( inputSplit.length <= 3) {
-        $scope.message = "Enjoy";
-        $scope.messgeColor = "green";
-      }else {
-        $scope.message = "Too much";
-      }
-
+    toBuy.bought = function (item){
+      ShoppingListCheckOffService.removeFromToBuyList(item);
     };
+
   }
+
+
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+  function AlreadyBoughtController(ShoppingListCheckOffService) {
+    var bought = this;
+    bought.items = ShoppingListCheckOffService.boughtList;
+  }
+
+
+  // crete shopping list service
+  function ShoppingListCheckOffService() {
+    var service = this;
+
+    service.listToBuy = listToBuy;
+    service.boughtList = [];
+
+    service.removeFromToBuyList = function (item){
+      // service.addToBuyList(item);
+      service.boughtList.push(service.listToBuy[item]);
+      service.listToBuy.splice(item, 1);
+      console.log("addToBoughtList: ", listToBuy[item]);
+      console.log("item removed: ", listToBuy[item]);
+    };
+
+  }
+
+  var listToBuy = [
+    {
+      name: "cookies",
+      quantity: 10
+    },
+    {
+      name: "chocolate bars",
+      quantity: 5
+    },
+    {
+      name: "coffe backs",
+      quantity: 8
+    },{
+      name: "sugar backs",
+      quantity: 7
+    },
+    {
+      name: "muffins",
+      quantity: 4
+    },
+    {
+      name: "croissant",
+      quantity: 4
+    }
+  ];
+
+  
 
 
 })();
